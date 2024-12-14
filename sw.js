e3866351df14b66ccdb8488f55708bd486597ec6
abaimov <w.abaimov@gmail.com>
2024-12-14 19:44:28 +0300

    fix sw

--- sw.js
+++ sw.js
@@ -1,53 +1,57 @@
-const staticCacheName = 's-app-v3'
-const dynamicCacheName = 'd-app-v3'
+const staticCacheName = 's-app-v3';
+const dynamicCacheName = 'd-app-v3';
 
 const assetUrls = [
   'index.html',
   '/js/app.js',
   '/css/styles.css',
-  'offline.html'
-]
+  'offline.html', // Статическая страница оффлайна
+];
 
 self.addEventListener('install', async event => {
-  const cache = await caches.open(staticCacheName)
-  await cache.addAll(assetUrls)
-})
+  const cache = await caches.open(staticCacheName);
+  await cache.addAll(assetUrls); // Добавляем статичные файлы в кэш
+});
 
 self.addEventListener('activate', async event => {
-  const cacheNames = await caches.keys()
+  const cacheNames = await caches.keys();
   await Promise.all(
-    cacheNames
-      .filter(name => name !== staticCacheName)
-      .filter(name => name !== dynamicCacheName)
-      .map(name => caches.delete(name))
-  )
-})
+      cacheNames
+          .filter(name => name !== staticCacheName)
+          .filter(name => name !== dynamicCacheName)
+          .map(name => caches.delete(name))
+  );
+});
 
 self.addEventListener('fetch', event => {
-  const {request} = event
+  const { request } = event;
+  const url = new URL(request.url);
 
-  const url = new URL(request.url)
   if (url.origin === location.origin) {
-    event.respondWith(cacheFirst(request))
+    // Если запрос на тот же домен (например, HTML, CSS, JS файлы)
+    event.respondWith(cacheFirst(request));
+  } else if (url.pathname.startsWith('/api/')) {
+    // Если запрос на динамические данные (например, API запросы)
+    event.respondWith(networkFirst(request));
   } else {
-    event.respondWith(networkFirst(request))
+    // Для всех остальных внешних запросов (например, картинки, шрифты)
+    event.respondWith(cacheFirst(request));
   }
-})
-
+});
 
 async function cacheFirst(request) {
-  const cached = await caches.match(request)
-  return cached ?? await fetch(request)
+  const cached = await caches.match(request);
+  return cached ?? await fetch(request); // Возвращаем из кэша или с сети
 }
 
 async function networkFirst(request) {
-  const cache = await caches.open(dynamicCacheName)
+  const cache = await caches.open(dynamicCacheName);
   try {
-    const response = await fetch(request)
-    await cache.put(request, response.clone())
-    return response
+    const response = await fetch(request);
+    await cache.put(request, response.clone()); // Сохраняем динамичные данные в кэш
+    return response; // Возвращаем данные из сети
   } catch (e) {
-    const cached = await cache.match(request)
-    return cached ?? await caches.match('/offline.html')
+    const cached = await cache.match(request);
+    return cached ?? await caches.match('/offline.html'); // Если нет интернета, показываем offline.html
   }
 }
